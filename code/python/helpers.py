95c302b1b2ed46d7124d01e74638c82e3343b808
Satwik Srivastava <satwiksri27@gmail.com>
2022-01-19 15:37:45 +0530

    Added Indexed Minimum Priority Queue for djikstra's algorithm

--- code/python/helpers.py
+++ code/python/helpers.py
@@ -1,3 +1,4 @@
+## === CLASS REPRESENTING AN EDGE === ##
 class Edge:
     def __init__(self, src, dest, cost=1):
         self.src = src
@@ -9,8 +10,9 @@ class Edge:
     
     def __str__(self) -> str:
         return repr(self)
-    
 
+    
+## === GRAPH DATA STRUCTURE === ##
 class Graph:
     def __init__(self, num_nodes, edges, directed=False):
         self.num_nodes = num_nodes
@@ -26,3 +28,104 @@ class Graph:
     
     def __str__(self) -> str:
         return repr(self)
+    
+
+## === INDEXED PRIORITY QUEUE USING MIN-HEAP === ##    
+class IndexedMinPQ:
+    def __init__(self, N) -> None:
+        self.N = N+1
+        self.key = [None for i in range(self.N)]
+        self.heap = [None for i in range(self.N)]
+        self.heap_index = [None for i in range(self.N)]
+        self.total = 0
+        
+    def push(self, idx, key):
+        if idx >= self.N:
+            raise IndexError('index is out of the range of IndexedMinPQ.')
+        if self.key[idx] is not None:
+            raise IndexError('index is already in the IndexedMinPQ.')
+
+        self.total += 1
+        self.key[idx] = key
+        self.heap[self.total] = idx
+        self.heap_index[idx] = self.total
+        self.__swim(self.total)
+        
+    def __swim(self, i):
+        parent_i = i//2
+        
+        while parent_i > 0:
+            key = self.key[self.heap[i]]
+            parent_key = self.key[self.heap[parent_i]]
+            if parent_key < key:
+                break
+            self.heap[i], self.heap[parent_i] = self.heap[parent_i], self.heap[i]
+            self.heap_index[self.heap[i]] , self.heap_index[self.heap[parent_i]] = \
+                self.heap_index[self.heap[parent_i]], self.heap_index[self.heap[i]]
+                
+            i = parent_i
+            parent_i = i//2
+            
+    def pop(self):
+        if not self.is_empty():
+            index = self.heap[1]
+            out = self.key[self.heap[1]]
+            self.key[self.heap[1]] = None
+            self.heap_index[self.heap[1]] = None
+            self.heap[1] = self.heap[self.total]
+            self.heap_index[self.heap[1]] = 1
+            self.heap[self.total] = None
+            self.total -= 1
+            self.__sink(1)
+            return index, out
+        raise IndexError('IndexedMinPQ is Empty')
+
+    def __sink(self,i):
+        child_i = i * 2
+        if child_i <= self.total:
+            key = self.key[self.heap[i]]
+            child_key = self.key[self.heap[child_i]]
+            other_child = child_i + 1
+            if other_child <= self.total:
+                other_child_key =  self.key[self.heap[other_child]]
+                if other_child_key < child_key:
+                    child_i = other_child
+                    child_key = other_child_key
+            if child_key < key:
+                self.heap[i], self.heap[child_i] = self.heap[child_i], self.heap[i]
+                self.heap_index[self.heap[i]], self.heap_index[self.heap[child_i]] = self.heap_index[self.heap[child_i]], self.heap_index[self.heap[i]]
+                self.__sink(child_i)
+
+    def is_empty(self):
+        return self.total == 0
+    
+    def update(self, i, key):
+        if key > self.key[i]:
+            self.__increase_key(i, key)
+            
+        else:
+            self.__decrease_key(i, key)
+                                              
+
+    def __decrease_key(self,i,key):
+        if i<0 or i> self.N:
+            raise IndexError('index i is not in the range')
+        if self.key[i] is None:
+            raise IndexError('index i is not in the IndexedMinPQ')
+        assert type(i) is int
+        assert key < self.key[i]
+        self.key[i] = key
+        self.__swim(self.heap_index[i])
+
+    def __increase_key(self,i,key):
+        if i<0 or i> self.N:
+            raise IndexError('index i is not in the range')
+        if self.key[i] is None:
+            raise IndexError('index i is not in the IndexedMinPQ')
+        assert type(i) is int
+        assert key > self.key[i]
+        self.key[i] = key
+        self.__sink(self.heap_index[i])
+        
+                     
+                                                                   
