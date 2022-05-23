5b4198a4baa5d31e6531a59ed26a705f168ec8ee
Satwik <satwiksri27@gmail.com>
2022-05-24 00:20:17 +0530

    Updated code

--- code/python/helpers.py
+++ code/python/helpers.py
@@ -1,36 +1,55 @@
+INF = float('inf')
+
+
 ## === CLASS REPRESENTING AN EDGE === ##
 class Edge:
     def __init__(self, src, dest, cost=1):
         self.src = src
         self.dest = dest
         self.cost = cost
-        
+
     def __repr__(self) -> str:
         return f"Edge({self.src}, {self.dest}, {self.cost}"
-    
+
     def __str__(self) -> str:
         return repr(self)
 
-    
+
 ## === GRAPH DATA STRUCTURE === ##
 class Graph:
-    def __init__(self, num_nodes, edges, directed=False):
+    def __init__(self, num_nodes, edges, directed=False, init_matrix=False):
         self.num_nodes = num_nodes
-        self.adj_list = {i: [] for i in range(self.num_nodes)}
-        
-        for edge in edges:
-            self.adj_list[edge.src].append((edge.dest, edge.cost))
-            if not directed:
-                self.adj_list[edge.dest].append((edge.src, edge.cost))
-                
+        self.num_edges = len(edges)
+        self.directed = directed
+        self.init_matrix = init_matrix
+
+        if not init_matrix:
+            self.adj_list = {i: [] for i in range(self.num_nodes)}
+
+            for edge in edges:
+                self.adj_list[edge.src].append((edge.dest, edge.cost))
+                if not directed:
+                    self.adj_list[edge.dest].append((edge.src, edge.cost))
+
+        else:
+            self.adj_matrix = [[INF for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+            for edge in edges:
+                self.adj_matrix[edge.src][edge.dest] = edge.cost
+                if not directed:
+                    self.adj_matrix[edge.dest][edge.src] = edge.cost
+
     def __repr__(self) -> str:
-        return f"{self.adj_list}"
-    
+        if self.init_matrix:
+            return f"{self.adj_matrix}"
+        else:
+            return f"{self.adj_list}"
+
     def __str__(self) -> str:
         return repr(self)
-    
 
-## === INDEXED PRIORITY QUEUE USING MIN-HEAP === ##    
+
+## === INDEXED PRIORITY QUEUE USING MIN-HEAP === ##
 class IndexedMinPQ:
     def __init__(self, N) -> None:
         self.N = N+1
@@ -38,7 +57,7 @@ class IndexedMinPQ:
         self.heap = [None for i in range(self.N)]
         self.heap_index = [None for i in range(self.N)]
         self.total = 0
-        
+
     def push(self, idx, key):
         if idx >= self.N:
             raise IndexError('index is out of the range of IndexedMinPQ.')
@@ -50,10 +69,10 @@ class IndexedMinPQ:
         self.heap[self.total] = idx
         self.heap_index[idx] = self.total
         self.__swim(self.total)
-        
+
     def __swim(self, i):
         parent_i = i//2
-        
+
         while parent_i > 0:
             key = self.key[self.heap[i]]
             parent_key = self.key[self.heap[parent_i]]
@@ -62,10 +81,10 @@ class IndexedMinPQ:
             self.heap[i], self.heap[parent_i] = self.heap[parent_i], self.heap[i]
             self.heap_index[self.heap[i]] , self.heap_index[self.heap[parent_i]] = \
                 self.heap_index[self.heap[parent_i]], self.heap_index[self.heap[i]]
-                
+
             i = parent_i
             parent_i = i//2
-            
+
     def pop(self):
         if not self.is_empty():
             index = self.heap[1]
@@ -98,14 +117,14 @@ class IndexedMinPQ:
 
     def is_empty(self):
         return self.total == 0
-    
+
     def update(self, i, key):
         if key > self.key[i]:
             self.__increase_key(i, key)
-            
+
         else:
             self.__decrease_key(i, key)
-                                              
+
 
     def __decrease_key(self,i,key):
         if i<0 or i> self.N:
@@ -126,6 +145,3 @@ class IndexedMinPQ:
         assert key > self.key[i]
         self.key[i] = key
         self.__sink(self.heap_index[i])
-        
-                     
-                                                                   
